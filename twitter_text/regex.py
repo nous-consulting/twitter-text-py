bbac0bea2ea5dffb4fa7c2b2f80bb2370180a1e3
Ignas Mikalajūnas <ignas.mikalajunas@gmail.com>
2011-10-04 19:22:56 +0200

    Bumped version, made protocols explicit (not just anything preceeding a colon).

--- twitter_text/regex.py
+++ twitter_text/regex.py
@@ -71,4 +71,34 @@ re.IGNORECASE + re.X)
 # 6 - Query string
 
 # Override with regexp from: http://daringfireball.net/2010/07/improved_regex_for_matching_urls
-REGEXEN['valid_url'] = re.compile(r"""((?:[^\/\"':!=]|^|\:))((?i)\b((?:[a-z][\w-]+:(?:/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])))""")
+# Override with regexp from: http://daringfireball.net/2010/07/improved_regex_for_matching_urls
+REGEXEN['valid_url'] = re.compile(r"""
+((?:[^\/\"':!=]|^|\:)) # Preceeding characters
+((?xi)
+\b
+(                           # Capture 1: entire matched URL
+  (?:
+    [http|https|ftp]+:                # URL protocol and colon
+    (?:
+      /{1,3}                        # 1-3 slashes
+      |                             #   or
+      [a-z0-9%]                     # Single letter or digit or '%'
+                                    # (Trying not to match e.g. "URI::Escape")
+    )
+    |                           #   or
+    www\d{0,3}[.]               # "www.", "www1.", "www2." … "www999."
+    |                           #   or
+    [a-z0-9.\-]+[.][a-z]{2,4}/  # looks like domain name followed by a slash
+  )
+  (?:                           # One or more:
+    [^\s()<>]+                      # Run of non-space, non-()<>
+    |                               #   or
+    \(([^\s()<>]+|(\([^\s()<>]+\)))*\)  # balanced parens, up to 2 levels
+  )+
+  (?:                           # End with:
+    \(([^\s()<>]+|(\([^\s()<>]+\)))*\)  # balanced parens, up to 2 levels
+    |                                   #   or
+    [^\s`!()\[\]{};:'".,<>?«»“”‘’]        # not a space or one of these punct chars
+  )
+))
+""", re.IGNORECASE + re.X)
